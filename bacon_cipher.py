55363af7dae71c06361006f428229975118809d9
kitter <79154704+buteler330@users.noreply.github.com>
2023-01-17 23:28:53 +0800

    use random text as covertext

--- bacon_cipher.py
+++ bacon_cipher.py
@@ -1,28 +1,12 @@
 import random
-print("require two arguments:  plaintext and covertext")
-print("WARNING: covertext's length should be strictly 5 times longer than the plaintext(or it gonna result in misdecrypted).space are allowed and will not count in length. no limitation on case")
+print("plaintext can only contain letter and space. not number or other punctuation")
 alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 plaintext=''
-covertext=""
-def inp():
-    global plaintext,covertext
-    plaintext=input("plaintext: ")
-    covertext=input("covertext(length should be 5 times longer than plaintext's length): ")
-inp()
-plaintext=plaintext.replace(" ","")
+plaintext=input("plaintext: ")
+plaintext_test=plaintext.replace(" ","")
 plaintext=plaintext.lower()
-covertext_test=covertext.replace(" ","")
-if (len(plaintext)!=(len(covertext_test))/5):
-    print("wrong length")
-    print(f"[length of plaintext=]={len(plaintext)}")
-    print(f"[length of covertext=]={len(covertext)}")
-    inp()
-
-# print(len(plaintext))
-# print(plaintext)
 key_map=[]
 key_source=["a","b"]
-
 # **因为不希望密码本里有重复部分，然后2^5=32,26个元素，很难没有，所以打算封装成函数，调用，if验证
 def gen_key_map():
         str=''
@@ -40,40 +24,52 @@ while i<26:
     else:
         key_map.append(str)
         i+=1
-# print(key_map)
-# print(len(key_map))
+print(key_map)
+# 转换plaintext
 trans_plain=''
 for h in range(len(plaintext)):
-    position=alpha.index(plaintext[h])
-    # print(position)
-    trans_plain+=key_map[position]
+    if plaintext[h]==" ":
+        trans_plain+=" "
+    else:   
+        position=alpha.index(plaintext[h])
+        trans_plain+=key_map[position]
 print("trans_plain:")
 print(trans_plain)
-trans_cover=''
+# 生成随机cover文本
+covertext=''
+for ind in range(5*len(plaintext_test)):
+    covertext+=alpha[random.randint(0,25)]
+print(f'covertext{covertext}')
+# 生成密文
 cou=0
-while cou<len(covertext) :
-    for u in range(len(trans_plain)):
-        if covertext[cou]==" ":
-            trans_cover+=" "
+covertext_count=0
+cipher=''
+while cou<len(trans_plain):
+    if trans_plain[cou]==" ":
+        cipher+=" "
+        cou+=1
+        if trans_plain[cou]=="b":
+            cipher+=covertext[covertext_count].upper()
             cou+=1
-            if trans_plain[u]=="b":
-                trans_cover+=covertext[cou].upper()
-                cou+=1
-            else:
-                trans_cover+=covertext[cou].lower()
-                cou+=1
-            continue
+            covertext_count+=1
         else:
-            if trans_plain[u]=="b":
-                trans_cover+=covertext[cou].upper()
-                cou+=1
-            else:
-                trans_cover+=covertext[cou].lower()
-                cou+=1
+            cipher+=covertext[covertext_count].lower()
+            cou+=1
+            covertext_count+=1
+        continue
+    else:
+        if trans_plain[cou]=="b":
+            cipher+=covertext[covertext_count].upper()
+            cou+=1
+            covertext_count+=1
+        else:
+            cipher+=covertext[covertext_count].lower()
+            cou+=1
+            covertext_count+=1
 print(f"[key map===>]{key_map}")
-print(f'[cipher===>][{trans_cover}]')
+print(f'[cipher===>][{cipher}]')
 
-
+# 因为covertext是随机生成的，所以不能添加空格，然而遍历的时候又是以原文的长度进行遍历，所以出现空格的时候会导致二者对应的错位，所以将每个计数器都单分出来
 
 
 
